617c6913eb221a9ab61067b11091db731a3b2af6
zhanghongbo <taobao1875@163.com>
2019-06-03 10:01:59 +0800

    init

---
+++ src/framework.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghongbo
+ * Date: 2019/6/3
+ * Time: 上午10:01
+ */
+
+class framework
+{
+    public function test()
+    {
+        echo "run framework";
+    }
+}
